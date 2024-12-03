8fcf47c09b75bd4a671f4d11e1a8dc0b98091b85
Nchimunya Munyama <61247975+nchimz101@users.noreply.github.com>
2024-12-03 09:49:05 +0300

    Update mapbox.ts

--- src/config/mapbox.ts
+++ src/config/mapbox.ts
@@ -1,5 +1,5 @@
 export const MAPBOX_CONFIG = {
-  accessToken: 'pk.eyJ1IjoibXVueWFtYTAxIiwiYSI6ImNsOXlvcWliYTAyZGYzd28wcDJnMzgxOWwifQ.JYkfcolUQTSk2jxJ0JVP_A',
+  accessToken: 'XXXXXXXXXXXXXXXXXXX',
   style: 'mapbox://styles/mapbox/streets-v12',
   defaultCenter: [-74.5, 40],
   defaultZoom: 9,
